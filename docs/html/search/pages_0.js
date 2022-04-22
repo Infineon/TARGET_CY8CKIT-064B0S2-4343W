90ebc5c1ce339d2ca3a46cce28add8f83573098c
gitlab-runner <do-not-reply@cypress.com>
2022-04-22 09:41:19 -0500

    Upload TARGET_CY8CKIT-064B0S2-4343W 3.1.0.27308

--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['cy8ckit_2d064b0s2_2d4343w_20bsp',['CY8CKIT-064B0S2-4343W BSP',['../index.html',1,'']]]
+  ['cy8ckit_2d064b0s2_2d4343w_20bsp_0',['CY8CKIT-064B0S2-4343W BSP',['../index.html',1,'']]]
 ];
